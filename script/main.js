3d3df665ee2d9e82e5325e27b678aa429bd8c379
youjy1201 <youjy1201@gmail.com>
2024-05-28 13:56:30 +0900

    start

---
+++ script/main.js
@@ -0,0 +1,110 @@
+// 메인 상단
+const heading = document.querySelector('.heading');
+const characters = heading.textContent.match(/[\w\W]/g);
+heading.textContent = '';
+
+characters.forEach(char => {
+  const span = document.createElement('span');
+  span.textContent = char;
+  heading.appendChild(span);
+});
+
+let char = 0;
+let timer = setInterval(setChar, 100);
+
+function setChar() {
+  const spanChar = document.querySelectorAll('span')[char];
+  spanChar.className = 'fade';
+  char++;
+  char === characters.length && clearInterval(timer);
+};
+
+var stars=800;
+var $stars=$(".stars");
+var r=800;
+for(var i=0;i<stars;i++){
+  var $star=$("<div/>").addClass("star");
+  $stars.append($star);
+}
+$(".star").each(function(){
+  var cur=$(this);
+  var s=0.2+(Math.random()*1);
+  var curR=r+(Math.random()*300);
+  cur.css({ 
+    transformOrigin:"0 0 "+curR+"px",
+    transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+  });
+});
+
+// 어바웃
+let arch_con = document.querySelectorAll(".arch_con");
+let arch_p = document.querySelectorAll(".arch_con div");
+let skil = document.querySelectorAll(".skil li");
+let skilText = document.querySelector(".arch .skil_t span");
+
+// 어바웃 슬라이드
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "1",
+  spaceBetween: 500,
+  autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: { // 아치 내용 변경
+    slideChange: function () {
+      arch_con.forEach((item) => {
+        item.classList.add("hidden");
+      });
+      arch_con[this.realIndex].classList.remove("hidden");
+    }
+  }
+});
+
+// 스킬 이름 변경
+skil.forEach((item, idx) => {
+  item.addEventListener("click", function () {
+    skilText.innerHTML = this.dataset.skil;
+    arch_p.forEach((item) => {
+      item.style.display = "none"
+    });
+    arch_p[idx].style.display = "block"
+  });
+});
+
+
+// 프로젝트 탭메뉴
+let tab = document.querySelectorAll("#projects .category label");
+let line = document.querySelector("#projects .line");
+let unit = document.querySelectorAll("#project .unit");
+
+// act 스타일 변경 함수
+const actStyle = (num) => {
+  let newLeft = tab[num].offsetLeft;
+  let newWidth = tab[num].offsetWidth;
+  line.style.left = newLeft + "px";
+  line.style.width = newWidth + "px";
+  tab.forEach((item) => {
+    item.style.fontWeight = "normal";
+  });
+  tab[num].style.fontWeight = "bold";
+}
+actStyle(0);
+
+tab.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    actStyle(idx);
+  });
+});
+
+
+// 프로젝트 주소 연결
+const project = document.querySelectorAll('.unit');
+
+project.forEach((item) => {
+  item.addEventListener('click', () => {
+    const link = item.dataset.id;
+    location.href = `project.html?${link}`;
+  });
+});
---
+++ script/project.js
@@ -0,0 +1,76 @@
+// 슬라이드
+$(function () {
+  $('.project_wrap').slick({
+    slidesToShow: 1,
+    // adaptiveHeight: true,
+    prevArrow: "<button type='button' class='slick-prev'><i class='fas fa-chevron-left'></i> Prev</button>",
+    nextArrow: "<button type='button' class='slick-next'>Next <i class='fas fa-chevron-right'></i></button>",
+    asNavFor: '.slide_nav',
+  });
+
+  // 내비 슬라이드
+  $('.slide_nav').slick({
+    slidesToShow: 3,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true,
+    asNavFor: '.project_wrap',
+    responsive: [ // 반응형 웹 구현 옵션
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+
+  $('.project_wrap').on('afterChange', function () {
+    $('html, body').animate({
+      scrollTop: 100
+    }, 300);
+
+    // 슬라이드 높이 변경
+    let currentSlide = $('.project_wrap').slick('slickCurrentSlide');
+
+    s_h = $('.project_wrap .slick-slide').eq(currentSlide + 1).outerHeight();
+
+    $('.project_wrap .slick-list').height(s_h);
+    // console.log(s_h);
+
+    return false;
+  });
+
+  p_id = $(location).attr('href').split('?')[1];
+  // console.log(p_id);
+
+  switch (p_id) {
+    case "miero":
+      $('.project_wrap').slick('slickGoTo', 0);
+      break
+
+    case "canon_user":
+      $('.project_wrap').slick('slickGoTo', 1);
+      break
+
+    case "canon_admin":
+      $('.project_wrap').slick('slickGoTo', 2);
+      break
+
+    case "hana":
+      $('.project_wrap').slick('slickGoTo', 3);
+      break
+
+    case "lg":
+      $('.project_wrap').slick('slickGoTo', 4);
+      break
+
+    case "tgi":
+      $('.project_wrap').slick('slickGoTo', 5);
+      break
+
+    default:
+      $('.project_wrap').slick('slickGoTo', 0);
+  }
+});
